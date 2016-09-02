da0d7e5021b6ca9af9d9f5be6fe67302875bdb88
DESKTOP-A823JR7\pushp <pushpindersingh135@gmail.com>
2016-09-01 21:44:45 -0400

    Moved some stuff around

---
+++ Assets/PlayerController/ControllerSwapper.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerSwapper : MonoBehaviour {
+
+    public GameObject normalController;
+    public GameObject gliderController;
+    public GameObject lastController;
+    public bool gliderMode = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+
+    void Update()
+    {
+        ChangeController();
+    }
+	
+	// Update is called once per frame
+	void ChangeController ()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Vector3 velocity = lastController.GetComponent<Rigidbody>().velocity;
+            Vector3 position = lastController.transform.position;
+            if (gliderMode)
+            {
+                Destroy(lastController);
+                lastController = Instantiate(normalController, position, Quaternion.Euler(0, 0, 0)) as GameObject;
+                lastController.GetComponent<Rigidbody>().velocity = velocity;
+            }
+            else
+            {
+                if (!lastController.GetComponent<UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController>().Grounded)
+                {
+                    return;
+                }
+                Destroy(lastController);
+                lastController = Instantiate(gliderController, position, Quaternion.Euler(0, 0, 0)) as GameObject;
+                lastController.transform.LookAt(transform.position + velocity);
+                lastController.GetComponent<Rigidbody>().velocity = velocity;
+            }
+        }	
+	}
+}
